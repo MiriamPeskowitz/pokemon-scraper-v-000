5b0677c1aafb6cb12f8efb63c9b4ed6fb9e837a5
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-05 01:05:12 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,28 +1,48 @@
-require 'pry'
+# require 'pry'
+# class Pokemon
+#   attr_accessor :id, :name, :type, :db
+# 
+# 
+#   def initialize(args)
+#     @name = args[:name]
+#     @type = args[:type]
+#     @db = args[:db]
+#     @id = args[:id]
+#   end
+# 
+#   def self.save(name, type, db)
+#       sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+#       db.execute(sql, name, type)
+#   end
+# 
+# 
+#   def self.find(id, db)
+#     sql = "SELECT * FROM pokemon WHERE id = ?"
+#     result = db.execute(sql, id).flatten #flatten returns array w/o nested arrays.
+#     Pokemon.new(id: result[0], name: result[1], type: result[2], db:db)
+#   end
+# end
+
 class Pokemon
-  attr_accessor :id, :name, :type, :db
-
-
-  def initialize(args)
-    @name = args[:name]
-    @type = args[:type]
-    @db = args[:db]
-    @id = args[:id]
-  end
+  attr_accessor :id, :name, :type, :hp, :db
 
   def self.save(name, type, db)
-      sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
-      db.execute(sql, name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
+  def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+  end
 
-  def self.find(id, db)
-    sql = "SELECT * FROM pokemon WHERE id = ?"
-    result = db.execute(sql, id).flatten #flatten returns array w/o nested arrays.
-    Pokemon.new(id: result[0], name: result[1], type: result[2], db:db)
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id, @name, @type, @hp, @db = id, name, type, hp, db
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 end
-
   # Pokemon.reify_from_row(row).flatten
   # finds based on id, returns a new Pokemon object, selects their row from the db, using the id number
   # def alter(new_health, db)
